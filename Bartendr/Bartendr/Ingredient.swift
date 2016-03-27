28640218865627cfad9b1de56f72173d68dc922a
mkausas <mkausas@gmail.com>
2016-03-26 22:35:07 -0400

    Added searching by ingredient types

--- Bartendr/Bartendr/Ingredient.swift
+++ Bartendr/Bartendr/Ingredient.swift
@@ -13,6 +13,25 @@ class Ingredient {
     var id: String?
     var text: String?
     
+//    static var BaseSpirit = "BaseSpirit"
+//    static var berries = "berries"
+//    static var brandy = "brandy"
+//    static var decoration = "decoration"
+//    static var fruits = "fruits"
+//    static var gin = "gin"
+//    static var ice = "ice"
+//    static var mixers = "mixers"
+//    static var others = "others"
+//    static var rum = "rum"
+//    static var spicesHerbs = "spices-herbs"
+//    static var spiritsOther = "spirits-other"
+//    static var tequila = "tequila"
+//    static var vodka = "vodka"
+//    static var whisky = "whisky"
+    
+    static var TYPES = ["BaseSpirit", "berries", "brandy", "decoration", "fruits", "gin", "ice", "mixers", "others", "rum", "spices-herbs", "spirits-other", "tequila", "vodka", "whisky"]
+    
+ 
     init(ingredientData: NSDictionary) {
         id = ingredientData["id"] as? String
         text = ingredientData["textPlain"] as? String
