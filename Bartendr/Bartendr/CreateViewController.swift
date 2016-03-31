a1cae5f5ad5d13e4f32a65b791599196f2753417
Lainie Wright <lainiewright@gmail.com>
2016-03-31 01:38:04 -0400

    updated saving drink

--- Bartendr/Bartendr/CreateViewController.swift
+++ Bartendr/Bartendr/CreateViewController.swift
@@ -16,11 +16,8 @@ class CreateViewController: UIViewController {
     @IBOutlet weak var bottomLabel: UILabel!
     @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var photoButtonView: UIView!
-    
-//    let ingredients = [["Brandy", "Gin", "Rum", "Tequila", "Vodka", "Whisky", "Vermouth"], ["Lemon Juice", "Lime Juice", "Cranberry Juice", "Pineapple Juice", "Orange Juice", "Tonic", "Grenadine", "Ginger Ale", "Cola"], ["Lime", "Lemon", "Orange", "Raspberry", "Strawberry", "Maraschino", "Pineapple"]]
-    
-//    let ingredients = ["Brandy", "Gin", "Rum", "Tequila", "Vodka", "Whisky", "Vermouth", "Lemon Juice", "Lime Juice", "Cranberry Juice", "Pineapple Juice", "Orange Juice", "Tonic", "Grenadine", "Ginger Ale", "Cola", "Lime", "Lemon", "Orange", "Raspberry", "Strawberry", "Maraschino", "Pineapple"]
-//    
+    @IBOutlet weak var nameField: UITextField!
+
     
     let ingredients = Ingredient.TYPES
     
@@ -52,77 +49,24 @@ class CreateViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//            return ingredients.count
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("CreateCell", forIndexPath: indexPath) as! CreateCell
-//        
-//        cell.nameLabel.text = ingredients[indexPath.row]
-//        cell.selectionStyle = .None
-//        cell.delegate = self
-//        
-//        cell.isSelected = checkStates[indexPath.row] ?? false
-//        
-//        if cell.isSelected == true {
-//            cell.checkBoxImageView.image = UIImage(named: "CheckBoxSelected")
-//        } else {
-//            cell.checkBoxImageView.image = UIImage(named: "CheckBox")
-//        }
-//        
-//        return cell
-//    }
-//    
-//    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-//        let footerView = tableView.dequeueReusableCellWithIdentifier("NameCell")! as UITableViewCell
-//        return footerView
-//    }
-//    
-//    func onPushCreate() {
-//        
-//        var ingredientList = [Ingredient]()
-//        for index in 0...checkStates.count {
-//            if checkStates[index] == true {
-//                ingredientList.append(Ingredient(ingredientData: ["text": ingredients[index]]))
-//            }
-//        }
-//        
-//        let name = "Another drink"
-//        let description = "description"
-//        let image = UIImage(named: "defaultDrink")
-//        
-//        let drink = Drink(name: name, description: description, customImg: image!, ingredients: ingredientList)
-//        
-//        
-//        ApiClient.createDrink(drink) { (success) in
-//            print("successfully created drink")
-//        }
-//        
-//    }
-//    
-////    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-////        return 50
-////    }
-//    
-//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        let cell = tableView.cellForRowAtIndexPath(indexPath) as! CreateCell
-//        
-//        cell.delegate?.createCell?(cell, didChangeValue: !cell.isSelected!)
-//        
-//        tableView.reloadData()
-//    }
-//    
-//    func createCell(createCell: CreateCell, didChangeValue value: Bool) {
-//        let indexPath = tableView.indexPathForCell(createCell)
-//        
-//        checkStates[indexPath!.row] = value
-//    }
+
+    @IBAction func onCreate(sender: AnyObject) {
+        let name = nameField.text! as String
+        let description = "description"
+        let image = UIImage(named: "defaultDrink")
+        
+        var ingredientList = [Ingredient]()
+        for ingredient in selectedIngredients {
+            ingredientList.append(Ingredient(ingredientData: ["text" : ingredient]))
+        }
+        
+        let drink = Drink(name: name, description: description, customImg: image!, ingredients: ingredientList)
+        
+        
+        ApiClient.createDrink(drink) { (success) in
+            print("successfully created drink")
+        }
+    }
 
     /*
     // MARK: - Navigation
